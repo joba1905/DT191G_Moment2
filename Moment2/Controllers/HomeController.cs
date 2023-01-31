c97adfc3f5d1c60866e239099116f3835fb007c8
Joacim Bäcklund <joba1905@student.miun.se>
2023-01-31 15:54:12 +0100

    Add project files.

---
+++ Moment2/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moment2.Models;
+using Newtonsoft.Json;
+
+namespace Moment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var jsonStr = System.IO.File.ReadAllText("bands.json");
+            var jsonObj = JsonConvert.DeserializeObject<List<Bands>>(jsonStr);
+
+            return View(jsonObj);
+        }
+
+        [Route("/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/add")]
+        public IActionResult Add(Bands model)
+        {
+            if (ModelState.IsValid)
+            {
+                var jsonStr = System.IO.File.ReadAllText("bands.json");
+                var jsonObj = JsonConvert.DeserializeObject<List<Bands>>(jsonStr);
+
+                if (jsonObj != null)
+                {
+                    jsonObj.Add(model);
+                    System.IO.File.WriteAllText("bands.json", JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+
+                    ModelState.Clear();
+                }
+            } 
+
+            return View();
+        }
+
+        [Route("/genres")]
+        public IActionResult Genres()
+        {
+            List<string> Genres = new List<string>
+            {
+                "Heavy metal",
+                "Power metal",
+                "Folk metal",
+                "Death metal",
+                "Black metal",
+                "Progressive metal"
+            };
+
+            ViewBag.Genres = Genres;
+            return View();
+        }
+    }
+}
---
+++ Moment2/Models/Bands.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moment2.Models
+{
+    public class Bands
+    {
+        [Required(ErrorMessage = "The band name is required!")]
+        [Display(Name = "Band name: *")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "A country is required!")]
+        [Display(Name = "From country: *")]
+        public string? Country { get; set; }
+
+        [Required(ErrorMessage = "The metal genre is required!")]
+        [Display(Name = "Genre of metal: *")]
+        public string? Genre { get; set; }
+
+        [Url]
+        [Display(Name = "Official band page:")]
+        public string? Url { get; set; }
+
+        [Required(ErrorMessage = "A year is required! Use format (YYYY)")]
+        [Display(Name = "Year formed: *")]
+        public string? Year { get; set; }
+
+        [MaxLength(1000)]
+        [Display(Name = "Band information:")]
+        public string? Description { get; set; }
+    }
+}
